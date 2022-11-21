1eb58f043fefe0b4147ab7d69f50ae32a576fc54
bilguun082 <102725539+bilguun082@users.noreply.github.com>
2022-11-21 19:44:54 +0800

    git status
    .

--- block1/DynamicContent/gif-page/src/App.js
+++ block1/DynamicContent/gif-page/src/App.js
@@ -1,32 +1,56 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Box, Container } from "@mui/material";
+import { Box } from "@mui/material";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import Gifs from "./components/gifs";
+import Button from "@mui/material/Button";
 
 function App() {
   const [gifs, setGifs] = useState([]);
-  useEffect(() => {
-    const Data = async () => {
-      try {
-        const res = await axios.get(
-          "https://api.giphy.com/v1/gifs/search?api_key=V04bmYdkIDL1LCJ5Q5ji3yBqfBoePq9G&q=hi&limit=25&offset=0&rating=pg&lang=en "
-        );
-        setGifs(res.data.data);
-        console.log(res);
-      } catch (error) {
-        console.log(error.message);
-      }
-    };
-    Data();
-  }, []);
+  const [inputValue, setInputValue] = useState("");
+  const Data = async () => {
+    try {
+      const res = await axios.get(
+        `https://api.giphy.com/v1/stickers/search?api_key=V04bmYdkIDL1LCJ5Q5ji3yBqfBoePq9G&q=${inputValue}&limit=25&offset=0&rating=r&lang=en`
+      );
+      setGifs(res.data.data);
+      console.log(res);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
-    <Box>
-      {gifs?.map((useGifs, index) => {
-        return (
-          <Box>
-            <img src={useGifs.bitly_gif_url} />
-          </Box>
-        );
-      })}
+    <Box
+      sx={{
+        width: "100vw",
+        padding: "100px",
+        dipslay: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundImage:
+          "linear-gradient(140deg, black 0%, blue 50%, grey 75%)",
+      }}
+    >
+      <form
+        onChange={(e) => {
+          Data();
+          setInputValue(e.target.value);
+        }}
+        style={{ marginBottom: "30px" }}
+      >
+        <OutlinedInput
+          type="search"
+          placeholder="Search..."
+          sx={{ color: "white" }}
+        />
+        <Button variant="contained">Search</Button>
+      </form>
+      <Box sx={{ width: "2000px", display: "flex", flexWrap: "wrap" }}>
+        {gifs?.map((useGifs, index) => {
+          return <Gifs useGifs={useGifs} index={index} key={index} />;
+        })}
+      </Box>
     </Box>
   );
 }
--- block1/DynamicContent/gif-page/src/components/gifs.jsx
+++ block1/DynamicContent/gif-page/src/components/gifs.jsx
@@ -1,2 +1,29 @@
+import * as React from "react";
+import { Box } from "@mui/material";
 
+export const Gifs = ({ useGifs }) => {
+  return (
+    <Box
+      sx={{
+        width: "400px",
+        height: "400px",
+        border: "3px solid black",
+        borderRadius: "10px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <p>{useGifs.title}</p>
+      <img
+        src={useGifs.images.fixed_height.url}
+        alt="gif"
+        width={300}
+        height={300}
+      />
+    </Box>
+  );
+};
 
+export default Gifs;
