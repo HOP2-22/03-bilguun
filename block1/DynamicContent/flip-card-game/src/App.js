8406a026722da7e384db87dc310feda938ddcf02
bilguun082 <102725539+bilguun082@users.noreply.github.com>
2022-12-05 17:25:58 +0800

    git status
    12.05

--- block1/DynamicContent/flip-card-game/src/App.js
+++ block1/DynamicContent/flip-card-game/src/App.js
@@ -1,22 +1,39 @@
 import React, { useState, useEffect } from "react";
-import dabi from "./pictures/fire.jpeg";
-import tsuyu from "./pictures/frog.png";
-import midoriya from "./pictures/midoriya.png";
-import bakugou from "./pictures/370237-Anime-My-Hero-Academia-Phone-Wallpaper,png.png";
 import cart from "./pictures/MV5BMjRlNTgwMWItYTAxMS00ODQ2LTlhOTMtOWRjMjlhYTQxNjlkXkEyXkFqcGdeQXVyMTMzNDExODE5._V1_FMjpg_UX1000_.jpg";
 import "./App.css";
-
+import fire from "./pictures/fire.jpeg";
+import frog from "./pictures/frog.png";
+import midoriya from "./pictures/midoriya.png";
+import bakugou from "./pictures/370237-Anime-My-Hero-Academia-Phone-Wallpaper,png.png";
+function* chunks(arr, n) {
+  for (let i = 0; i < arr.length; i += n) {
+    yield arr.slice(i, i + n);
+  }
+}
 function App() {
+  const [cards, setCards] = useState([]);
+  const [flip, setFlip] = useState(0);
   const characterImages = [
-    { src: "/pictures/fire.jpeg" },
-    { src: "/pictures/frog.png" },
-    { src: "/pictures/midoriya.png" },
-    { src: "/pictures/370237-Anime-My-Hero-Academia-Phone-Wallpaper,png.png" },
+    {
+      src: fire,
+    },
+    {
+      src: frog,
+    },
+    {
+      src: midoriya,
+    },
+    {
+      src: bakugou,
+    },
   ];
+
   const generate = () => {
     let a = [...characterImages, ...characterImages].sort(
       () => Math.random() - 0.5
     );
+    setCards([...chunks(a, 2)]);
+    setFlip(0);
   };
   return (
     <div
@@ -24,9 +41,19 @@ function App() {
         width: "100vw",
         height: "100vh",
         display: "flex",
-        flexDirection: "column",
+        flexDirection: "row",
       }}
-    ></div>
+    >
+      {cards.map((card, index) => {
+        return (
+          <div>
+            <img src={card.src} alt="front" width={200} height={350} />
+            <img src={cart} alt="back" width={200} height={350} />
+          </div>
+        );
+      })}
+      <button onClick={generate}>generate</button>
+    </div>
   );
 }
 
