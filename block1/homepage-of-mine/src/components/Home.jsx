15d9a4584b3b30b7a18402a9b4df3cf5b42115a3
bilguun082 <102725539+bilguun082@users.noreply.github.com>
2023-02-02 22:54:42 +0800

    firebase done

--- block1/homepage-of-mine/src/components/Home.jsx
+++ block1/homepage-of-mine/src/components/Home.jsx
@@ -1,4 +1,6 @@
-import React, { useContext } from "react";
+import React, { Suspense, useContext, useRef } from "react";
+import { Canvas } from "@react-three/fiber";
+import { useGLTF, useAnimations, OrbitControls } from "@react-three/drei";
 import { Box } from "@mui/system";
 import { Typography } from "@mui/material";
 import Container from "@mui/material/Container";
@@ -6,9 +8,107 @@ import Profile from "../Bilguun.jpeg";
 import Github from "../assets/github.svg";
 import Instagram from "../assets/instagram.svg";
 import { ColorModeContext } from "../context/context";
-import Lottie from "lottie-react";
-import CodingBoyAnimation from "../assets/95348-coding-boy.json";
-// import { ThreeJS } from "../components/three";
+// import Lottie from "lottie-react";
+// import CodingBoyAnimation from "../assets/95348-coding-boy.json";
+import { AmbientLight } from "three";
+function NestBird(props) {
+  const group = useRef();
+  const { nodes, materials, animations } = useGLTF("/NestBird.gltf");
+  const { actions } = useAnimations(animations, group);
+  return (
+    <group ref={group} {...props} dispose={null}>
+      <group name="Scene">
+        <group name="Armature" rotation={[Math.PI / 2, 0, 0]}>
+          <primitive object={nodes.mixamorigHips} />
+          <group name="Cube001">
+            <skinnedMesh
+              name="Cube005"
+              geometry={nodes.Cube005.geometry}
+              material={materials["Material.008"]}
+              skeleton={nodes.Cube005.skeleton}
+            />
+            <skinnedMesh
+              name="Cube005_1"
+              geometry={nodes.Cube005_1.geometry}
+              material={materials["Material.009"]}
+              skeleton={nodes.Cube005_1.skeleton}
+            />
+            <skinnedMesh
+              name="Cube005_2"
+              geometry={nodes.Cube005_2.geometry}
+              material={materials["Material.010"]}
+              skeleton={nodes.Cube005_2.skeleton}
+            />
+            <skinnedMesh
+              name="Cube005_3"
+              geometry={nodes.Cube005_3.geometry}
+              material={materials["Material.011"]}
+              skeleton={nodes.Cube005_3.skeleton}
+            />
+            <skinnedMesh
+              name="Cube005_4"
+              geometry={nodes.Cube005_4.geometry}
+              material={materials["Material.012"]}
+              skeleton={nodes.Cube005_4.skeleton}
+            />
+            <skinnedMesh
+              name="Cube005_5"
+              geometry={nodes.Cube005_5.geometry}
+              material={materials["Material.013"]}
+              skeleton={nodes.Cube005_5.skeleton}
+            />
+          </group>
+        </group>
+        <group name="Cube" scale={[0.98, 3.83, 0.98]}>
+          <mesh
+            name="Cube001_1"
+            castShadow
+            receiveShadow
+            geometry={nodes.Cube001_1.geometry}
+            material={materials["Material.001"]}
+          />
+          <mesh
+            name="Cube001_2"
+            castShadow
+            receiveShadow
+            geometry={nodes.Cube001_2.geometry}
+            material={materials["Material.002"]}
+          />
+          <mesh
+            name="Cube001_3"
+            castShadow
+            receiveShadow
+            geometry={nodes.Cube001_3.geometry}
+            material={materials["Material.003"]}
+          />
+          <mesh
+            name="Cube001_4"
+            castShadow
+            receiveShadow
+            geometry={nodes.Cube001_4.geometry}
+            material={materials["Material.004"]}
+          />
+          <mesh
+            name="Cube001_5"
+            castShadow
+            receiveShadow
+            geometry={nodes.Cube001_5.geometry}
+            material={materials["Material.005"]}
+          />
+          <mesh
+            name="Cube001_6"
+            castShadow
+            receiveShadow
+            geometry={nodes.Cube001_6.geometry}
+            material={materials["Material.006"]}
+          />
+        </group>
+      </group>
+    </group>
+  );
+}
+
+useGLTF.preload("/NestBird.gltf");
 export const Home = () => {
   const { theme } = useContext(ColorModeContext);
   const styles = {
@@ -56,8 +156,21 @@ export const Home = () => {
     <Box sx={styles.body}>
       <Container maxWidth="sm">
         <Box>
-          {/* <ThreeJS /> */}
-          <Lottie animationData={CodingBoyAnimation} loop={true} />
+          <Box sx={{ width: "80%", height: "40vh" }}>
+            <Canvas>
+              <Suspense fallback={null}>
+                <ambientLight intensity={0.6} />
+                <directionalLight intensity={0.5} />
+                <NestBird />
+                <OrbitControls
+                  enablePan={true}
+                  enableZoom={true}
+                  enableRotate={true}
+                />
+              </Suspense>
+            </Canvas>
+          </Box>
+          {/* <Lottie animationData={CodingBoyAnimation} loop={true} /> */}
           <Box sx={styles.firstRow}>
             <Box>
               <Typography sx={styles.portfolio}>
