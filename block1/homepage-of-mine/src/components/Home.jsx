f10ddf573927b3d7cf26e885342b528dd74cc1ce
bilguun082 <bilguune060829@gmail.com>
2023-02-06 19:32:36 +0800

    finish css

--- block1/homepage-of-mine/src/components/Home.jsx
+++ block1/homepage-of-mine/src/components/Home.jsx
@@ -1,120 +1,19 @@
 import React, { Suspense, useContext, useRef } from "react";
 import { Canvas } from "@react-three/fiber";
-import { useGLTF, useAnimations, OrbitControls } from "@react-three/drei";
-import { Box } from "@mui/system";
+import { OrbitControls } from "@react-three/drei";
 import { Typography } from "@mui/material";
 import Container from "@mui/material/Container";
 import Profile from "../Bilguun.jpeg";
-import Github from "../assets/github.svg";
-import Instagram from "../assets/instagram.svg";
 import { ColorModeContext } from "../context/context";
-// import Lottie from "lottie-react";
-// import CodingBoyAnimation from "../assets/95348-coding-boy.json";
-import { AmbientLight } from "three";
-function NestBird(props) {
-  const group = useRef();
-  const { nodes, materials, animations } = useGLTF("/NestBird.gltf");
-  const { actions } = useAnimations(animations, group);
-  return (
-    <group ref={group} {...props} dispose={null}>
-      <group name="Scene">
-        <group name="Armature" rotation={[Math.PI / 2, 0, 0]}>
-          <primitive object={nodes.mixamorigHips} />
-          <group name="Cube001">
-            <skinnedMesh
-              name="Cube005"
-              geometry={nodes.Cube005.geometry}
-              material={materials["Material.008"]}
-              skeleton={nodes.Cube005.skeleton}
-            />
-            <skinnedMesh
-              name="Cube005_1"
-              geometry={nodes.Cube005_1.geometry}
-              material={materials["Material.009"]}
-              skeleton={nodes.Cube005_1.skeleton}
-            />
-            <skinnedMesh
-              name="Cube005_2"
-              geometry={nodes.Cube005_2.geometry}
-              material={materials["Material.010"]}
-              skeleton={nodes.Cube005_2.skeleton}
-            />
-            <skinnedMesh
-              name="Cube005_3"
-              geometry={nodes.Cube005_3.geometry}
-              material={materials["Material.011"]}
-              skeleton={nodes.Cube005_3.skeleton}
-            />
-            <skinnedMesh
-              name="Cube005_4"
-              geometry={nodes.Cube005_4.geometry}
-              material={materials["Material.012"]}
-              skeleton={nodes.Cube005_4.skeleton}
-            />
-            <skinnedMesh
-              name="Cube005_5"
-              geometry={nodes.Cube005_5.geometry}
-              material={materials["Material.013"]}
-              skeleton={nodes.Cube005_5.skeleton}
-            />
-          </group>
-        </group>
-        <group name="Cube" scale={[0.98, 3.83, 0.98]}>
-          <mesh
-            name="Cube001_1"
-            castShadow
-            receiveShadow
-            geometry={nodes.Cube001_1.geometry}
-            material={materials["Material.001"]}
-          />
-          <mesh
-            name="Cube001_2"
-            castShadow
-            receiveShadow
-            geometry={nodes.Cube001_2.geometry}
-            material={materials["Material.002"]}
-          />
-          <mesh
-            name="Cube001_3"
-            castShadow
-            receiveShadow
-            geometry={nodes.Cube001_3.geometry}
-            material={materials["Material.003"]}
-          />
-          <mesh
-            name="Cube001_4"
-            castShadow
-            receiveShadow
-            geometry={nodes.Cube001_4.geometry}
-            material={materials["Material.004"]}
-          />
-          <mesh
-            name="Cube001_5"
-            castShadow
-            receiveShadow
-            geometry={nodes.Cube001_5.geometry}
-            material={materials["Material.005"]}
-          />
-          <mesh
-            name="Cube001_6"
-            castShadow
-            receiveShadow
-            geometry={nodes.Cube001_6.geometry}
-            material={materials["Material.006"]}
-          />
-        </group>
-      </group>
-    </group>
-  );
-}
-
-useGLTF.preload("/NestBird.gltf");
+import { Bio } from "./Bio";
+import { OnTheWeb } from "./OnTheWeb";
+import { Box } from "@mui/system";
+import { Bird } from "./Bird";
 export const Home = () => {
   const { theme } = useContext(ColorModeContext);
   const styles = {
     firstRow: {
       width: "100%",
-      height: "100%",
       display: "flex",
       flexDirection: "row",
       alignItems: "center",
@@ -132,6 +31,7 @@ export const Home = () => {
       textDecorationColor: "#525252",
       textDecorationThickness: "4px",
       color: theme === "white" ? "white" : "black",
+      marginBottom: "20px",
     },
     webs: {
       marginBottom: "10px",
@@ -143,7 +43,6 @@ export const Home = () => {
     },
     body: {
       width: "100%",
-      height: "100vh",
       backgroundColor: theme === "white" ? "black" : "white",
     },
     portfolio: {
@@ -151,26 +50,33 @@ export const Home = () => {
       color: theme === "white" ? "white" : "black",
       fontSize: "25px",
     },
+    pic: {
+      width: "70px",
+      height: "70px",
+      overflow: "hidden",
+      borderRadius: "100px",
+      border: theme === "white" ? "1px solid white" : "1px solid black",
+    },
   };
   return (
     <Box sx={styles.body}>
       <Container maxWidth="sm">
         <Box>
-          <Box sx={{ width: "80%", height: "40vh" }}>
-            <Canvas>
-              <Suspense fallback={null}>
-                <ambientLight intensity={0.6} />
-                <directionalLight intensity={0.5} />
-                <NestBird />
-                <OrbitControls
-                  enablePan={true}
-                  enableZoom={true}
-                  enableRotate={true}
-                />
-              </Suspense>
-            </Canvas>
+          <Bird />
+          <Box
+            borderRadius="lg"
+            mb={6}
+            p={3}
+            textAlign="center"
+            sx={{
+              backdropFilter: "blur(10px)",
+              backgroundColor: "rgba(255, 255, 255, 0.25)",
+              borderRadius: "100px",
+              marginTop: "-120px",
+            }}
+          >
+            Hello, I&apos;m an indie app developer based in Japan!
           </Box>
-          {/* <Lottie animationData={CodingBoyAnimation} loop={true} /> */}
           <Box sx={styles.firstRow}>
             <Box>
               <Typography sx={styles.portfolio}>
@@ -178,31 +84,12 @@ export const Home = () => {
               </Typography>
               <Typography sx={styles.font}>Student of Pinecone</Typography>
             </Box>
-            <Box
-              sx={{
-                width: "70px",
-                height: "70px",
-                overflow: "hidden",
-                borderRadius: "100px",
-                border:
-                  theme === "white" ? "1px solid white" : "1px solid black",
-              }}
-            >
+            <Box sx={styles.pic}>
               <img src={Profile} alt="profile" width={70} height={70} />
             </Box>
           </Box>
           <Box>
-            <Box sx={{ marginBottom: "40px" }}>
-              <Typography sx={styles.infos} variant="h5">
-                Bio
-              </Typography>
-              <Typography sx={styles.font}>
-                2006 - born in Ulaanbaatar, Mongolia
-              </Typography>
-              <Typography sx={styles.font}>
-                2021 - Present - Learning as a programmer in Pinecone Academy
-              </Typography>
-            </Box>
+            <Bio />
             <Box sx={{ marginBottom: "40px" }}>
               <Typography sx={styles.infos} variant="h5">
                 I♥
@@ -211,48 +98,7 @@ export const Home = () => {
                 Music, Basketball, Playing games, Volleyball, Writing code
               </Typography>
             </Box>
-            <Typography sx={styles.infos} variant="h5">
-              On the web
-            </Typography>
-            <Box sx={styles.webs}>
-              <img src={Github} alt="git" />
-              <a
-                href="https://github.com/bilguun082"
-                style={{
-                  textDecoration: "none",
-                  color: theme === "white" ? "white" : "black",
-                  marginLeft: "10px",
-                }}
-              >
-                @bilguun082
-              </a>
-            </Box>
-            <Box sx={styles.webs}>
-              <img src={Instagram} alt="git" />
-              <a
-                href="https://github.com/bilguun082"
-                style={{
-                  textDecoration: "none",
-                  color: theme === "white" ? "white" : "black",
-                  marginLeft: "10px",
-                }}
-              >
-                @bilguun.e_29
-              </a>
-            </Box>
-            <Box sx={styles.webs}>
-              <img src={Instagram} alt="git" />
-              <a
-                href="https://github.com/bilguun082"
-                style={{
-                  textDecoration: "none",
-                  color: theme === "white" ? "white" : "black",
-                  marginLeft: "10px",
-                }}
-              >
-                @Билгүүн Э.
-              </a>
-            </Box>
+            <OnTheWeb />
           </Box>
         </Box>
       </Container>
